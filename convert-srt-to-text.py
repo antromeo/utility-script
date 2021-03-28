47dd7dddc0abab1135bd2d4b48830273957d5b40
antromeo <45900043+antromeo@users.noreply.github.com>
2021-03-28 10:31:59 +0200

    add nonewline option

--- convert-srt-to-text.py
+++ convert-srt-to-text.py
@@ -3,7 +3,11 @@ import sys
 import re
 
 def main():
-	if len(sys.argv)!=2:
+	if(sys.argv[-1]=='--help'):
+		print("input valid example: python convert-srt-to-text.py file.srt")
+		print("--nonewline append text without newline")
+		return
+	if len(sys.argv)!=2 and len(sys.argv)!=3:
 		print("input valid example: python convert-srt-to-text.py file.srt")
 		return 
 	#read file line by line
@@ -16,7 +20,11 @@ def main():
 		if re.search('^[0-9]+$', line) is None and re.search('^[0-9]{2}:[0-9]{2}:[0-9]{2}',line) is None and re.search('^$', line) is None:
 			text+=' ' + line
 	namefile = str(sys.argv[1]);
-	outputfile = open("output-"+os.path.basename(namefile[:-4])+ ".txt", "w")
+	outputfile = open("output-"+os.path.basename(namefile[:-4])+ ".txt", "w+")
 	outputfile.write(text)
+	if sys.argv[-1]=='--nonewline':
+		nonewline=text.replace('\n', ' ').replace('\r', ' ')
+		outputfile.write("\n\nTEXT WITHOUT NEWLINE:\n\n"+nonewline)
 	print("DONE!!!")
+	outputfile.close()
 main()
